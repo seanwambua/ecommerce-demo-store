fec5ea507b8aaeffc53f68f48ff56b1ea45bf330
seanwambua <seanwambua@gmail.com>
2026-08-15 05:16:15 +0300

    fix: update import path for utility function in Drawer component

--- src/components/ui/drawer.tsx
+++ src/components/ui/drawer.tsx
@@ -1,7 +1,7 @@
 import * as React from "react"
 import { Drawer as DrawerPrimitive } from "@base-ui/react/drawer"
 
-import { cn } from "src/lib/utils"
+import { cn } from "../../lib/utils"
 
 type DrawerContextProps = {
   hasSnapPoints: boolean
